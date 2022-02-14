3d0eddd92214921459390a90bed9f6fa99b685f9
Doron Marcus <doron.marcus@gmail.com>
2022-02-14 15:27:28 +0200

    update script

--- scripts/script.py
+++ scripts/script.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import argparse
+import sys
+print('Executing script file is:', str(sys.argv[0]))
+print('The arguments are:', str(sys.argv))
+parser = argparse.ArgumentParser()
+parser.add_argument("--world", help="Provide the name of the world to greet.")
+args = parser.parse_args()
+print('Hello ', args.world)
